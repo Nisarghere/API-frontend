517e20a4d53371cd5ca0a419b6af99132e42656c
Nisarghere <nisarg72020@gmail.com>
2026-08-09 23:59:46 +0530

    categories added

--- app/Explore/Explore.tsx
+++ app/Explore/Explore.tsx
@@ -6,18 +6,33 @@ const Explore = () => {
   return (
     <div>
         
-        <div className='grid grid-cols-[1fr_3fr] min-h-screen'>
-          <div className='border border-t-0 border-b-0 border-l-0  h-full'>
-            section 1
+        <div className='grid grid-cols-[1fr_3fr] min-h-screen border border-[#E4E4E7]'>
+          <div className='border border-t-0 border-b-0 border-[#c9c9fa] border-l-0  h-full'>
+    <div className='flex flex-col p-6'>
+      <h2 className='text-sm font-bold text-[#71717A]'>Categories</h2>
+    </div>
+            <div className=' border border-[#E4E4E7] ml-2 mr-2 shrink ' />
+
+            <div className='h-full p-6  mt-5  justify-start text-left'>
+
+            <ul className='text-left  flex flex-col  '>
+              <li className='text-sm font-semibold hover:bg-[#e9fae9] cursor-pointer hover:-translate-y-0.5 transition duration-300 py-3 px-2'>Cybersecurity</li>
+              <li className='text-sm font-semibold hover:bg-[#e9fae9] cursor-pointer hover:-translate-y-0.5 transition duration-300 py-3 px-2'>Finance</li>
+              <li className='text-sm font-semibold hover:bg-[#e9fae9] cursor-pointer hover:-translate-y-0.5 transition duration-300 py-3 px-2'>Weather</li>
+              <li className='text-sm font-semibold hover:bg-[#e9fae9] cursor-pointer hover:-translate-y-0.5 transition duration-300 py-3 px-2'>Translator</li>
+              <li className='text-sm font-semibold hover:bg-[#e9fae9] cursor-pointer hover:-translate-y-0.5 transition duration-300 py-3 px-2'>Movies</li>
+              <li className='text-sm font-semibold hover:bg-[#e9fae9] cursor-pointer hover:-translate-y-0.5 transition duration-300 py-3 px-2'>Cryptography</li>
+            </ul>
+            </div>
           </div>
-          <div className='border'>
+          {/* <div className='border'>
             <SearchBar/>
             <div className='flex'>
               <APicard/>
               <APicard/>
               <APicard/>
-            </div>
-          </div>
+            </div> */}
+          {/* </div> */}
         </div>
     </div>
   )
